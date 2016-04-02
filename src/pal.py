2cce3b77ec4ff5a64663148e0edf532f10a9f9ac
Sankha Subhra Mukherjee <sankha.mukherjee@gmail.com>
2016-04-02 20:43:01 +0800

    with the 3 string comparison palindrome

--- src/pal.py
+++ src/pal.py
@@ -21,7 +21,7 @@ def inputString():
 
 def findPalindrome2(string):
     '''
-        Thsi function is used for finding the biggest
+        This function is used for finding the biggest
         palindrome within a string. It does a grouping
         by 2 ...
     '''
@@ -41,24 +41,61 @@ def findPalindrome2(string):
         if len(bigPals) < len(string[i:i+2]):
             bigPals = string[i:i+2]
        
+
+        # print  i, len(string)-i, i+1
  
-        for j in range(2, min( len(string)-i, i )):
-            # print string[i-j+1: i+j+1]
+        for j in range(1, len(string)):
+
+            if i-j+1 < 0 : break
+            if i+j >= len(string): break
+
             if string[i-j+1] != string[i+j]:
                 break
             else:
                 if len(bigPals) < len(string[i-j+1: i+j+1]):
                     bigPals = string[i-j+1: i+j+1]
- 
+
+    if bigPals == '': return None
+
+    return bigPals
+
+def findPalindrome3(string):
+    '''
+        This function finds the biggest palindrome
+        in a string by starting with groupings of 3
+    '''
+
+    if len(string) < 3: return None
+
+    bigPals = ''
+    for i in range(1, len(string)-2):
+        if string[i-1] != string[i+1]: continue
+
+        # We are in palindrome territory ...
+        if len(string[i-1:i+1+1]) > bigPals: bigPals = string[i-1:i+1+1]
+        # print string[i-1:i+1+1]
+        for j in range(2, len(string)):
+            if i-j < 0: break
+            if i+j >= len(string): break
+            if string[i-j] != string[i+j]: break
+            if len(string[i-j:i+j+1]) > len(bigPals): bigPals = string[i-j:i+j+1]
+            # print string[i-j], string[i+j], string[i-j:i+j+1]
+
+    if bigPals == '': return None
     return bigPals
 
 
 if __name__ == '__main__':
  
     inp = inputString()
-    print inp
-    print
+    print zip(inp, range(len(inp)))
+    
+    print 'Finding for 2'
     print findPalindrome2(inp)
+
+    print 'Finding for 3'
+    print findPalindrome3(inp)
+
  
     print 'done'
 
